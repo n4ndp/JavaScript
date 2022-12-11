297e85331131b5d077acd8f010990adf8c114c37
n4ndp <fer.choqque.megmail.com>
2022-12-11 04:58:59 -0500

    remasterrrr

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Aprediendo JavaScript
 # **Referencias Bibliográficas**
 
 - [MDN Web Docs](https://developer.mozilla.org/es/)
+- [JavaScript](https://jonmircha.com/javascript)
+- [ECMAScript](https://jonmircha.com/ecmascript)
---
+++ Tipos de Datos/array.js
@@ -0,0 +1,13 @@
+const arr1 = [];
+const arr2 = [1, true, "true"];
+const arr3 = [1, true, "true", [1, true, "true"]];
+
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+
+console.log(arr2.length);
+console.log(arr2[1]);
+console.log(arr3[3][2]);
+
+const arr4 = Array.of("a", "e", "i", "o", "u", 1, 2, 3, 4, 5);
